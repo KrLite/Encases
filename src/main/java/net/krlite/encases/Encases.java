93974b3f7ec84adf86b50f630ec7f2c321971321
KrLite <68179735+KrLite@users.noreply.github.com>
2023-05-08 20:00:14 +0800

    copy contents, no mojmap

--- src/main/java/net/krlite/encases/Encases.java
+++ src/main/java/net/krlite/encases/Encases.java
@@ -2,18 +2,25 @@ package net.krlite.encases;
 
 import com.simibubi.create.Create;
 
+import com.simibubi.create.foundation.data.CreateRegistrate;
+
 import io.github.fabricators_of_create.porting_lib.util.EnvExecutor;
 import net.fabricmc.api.ModInitializer;
 
+import net.krlite.encases.util.EncasesItemGroups;
 import net.minecraft.resources.ResourceLocation;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 public class Encases implements ModInitializer {
-	public static final String ID = "modid";
-	public static final String NAME = "Example Mod";
+	public static final String NAME = "Create: Encases", ID = "encases";
 	public static final Logger LOGGER = LoggerFactory.getLogger(NAME);
+	public static final CreateRegistrate REGISTRATE = CreateRegistrate.create(ID);
 
 	@Override
 	public void onInitialize() {
@@ -22,6 +29,14 @@ public class Encases implements ModInitializer {
 				() -> () -> "{} is accessing Porting Lib from the client!",
 				() -> () -> "{} is accessing Porting Lib from the server!"
 		), NAME);
+	}
+
+	public static CreateRegistrate registrate() {
+		return REGISTRATE.creativeModeTab(() -> EncasesItemGroups.CREATORS_DECORS);
+	}
+
+	public static Identifier identifier(String... paths) {
+		return new Identifier(Arrays.stream(paths).filter(Objects::nonNull).filter(p -> !p.isEmpty()).collect(Collectors.joining("/")));
 	}
 
 	public static ResourceLocation id(String path) {
---
+++ src/main/java/net/krlite/encases/EncasesAllItems.java
@@ -0,0 +1,6 @@
+package net.krlite.encases;
+
+import com.simibubi.create.AllItems;
+
+public class EncasesAllItems extends AllItems {
+}
